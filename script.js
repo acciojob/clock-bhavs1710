75a372ac7a07da0a8e1211a6041e57b06d4d6d9f
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-07-21 15:02:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //your JS code here. If required.
-const time= document.getElementById("timer");
-SetInterval(() => {
-	let d= new Date();
-	console.log(d);
-	time.innerText= d.getSeconds();
-	time.innerText= d.getMinutes();
-	time.innerText= d.getHours();
-},1000)
+function time(){
+let date= new Date();
+let hour = date.getHours();
+let min= date.getMinutes();
+let sec=date.getSeconds();
+
+let session="AM";
+if(hour==0){
+    hour=12;//12 hour format
+}
+if(hour>12){
+    hour=hour-12;
+    session="PM";
+}
+// if(hour,min,sec<10){
+//     hour="0"+hour;
+//     min="0"+min;
+//     sec="0"+sec;
+// }
+hour=(hour<10)?"0"+hour:hour;
+min=(min<10)?"0"+min:min;
+sec=(sec<10)?"0"+sec:sec;
+
+document.getElementById("timer").innerHTML= date , hour+":"+min+":"+sec+" "+session; 
+
+setTimeout(time,1000);
+}
+
+time();
